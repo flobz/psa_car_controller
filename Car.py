af77cd90c28b3a236fccab5b072263224ce35702
Florian Bezannier <florian.bezannier@hotmail.fr>
2021-04-10 17:56:00 +0200

    find model by vin

--- Car.py
+++ Car.py
@@ -2,82 +2,33 @@ import json
 from copy import copy
 
 from MyLogger import logger
+from libs.car_model import CarModel
 from libs.car_status import CarStatus
 
-ENERGY_CAPACITY = {'SUV 3008': {'BATTERY_POWER': 10.8, 'FUEL_CAPACITY': 43},
-                   'C5 Aircross': {'BATTERY_POWER': 10.8, 'FUEL_CAPACITY': 43},
-                   'e-208': {'BATTERY_POWER': 46, 'FUEL_CAPACITY': 0, "ABRP_NAME": "peugeot:e208:20:50"},
-                   'e-2008': {'BATTERY_POWER': 46, 'FUEL_CAPACITY': 0, "ABRP_NAME": "peugeot:e2008:20:48"},
-                   'corsa-e': {'BATTERY_POWER': 46, 'FUEL_CAPACITY': 0, "ABRP_NAME": "opel:corsae:20:50"}
-                   }
-DEFAULT_BATTERY_POWER = 46
-DEFAULT_FUEL_CAPACITY = 0
-DEFAULT_MAX_ELEC_CONSUMPTION = 70
-DEFAULT_MAX_FUEL_CONSUMPTION = 30
 CARS_FILE = "cars.json"
 
 
 class Car:
-    def __init__(self, vin, vehicle_id, brand, label="unknown", battery_power=None, fuel_capacity=None,
+    def __init__(self, vin, vehicle_id, brand, label=None, battery_power=None, fuel_capacity=None,
                  max_elec_consumption=None, max_fuel_consumption=None, abrp_name=None):
         self.vin = vin
+        if label is not None:
+            model = CarModel.find_model_by_name(label)
+        else:
+            model = CarModel.find_model_by_vin(self.vin)
+            label = model.name
         self.vehicle_id = vehicle_id
         self.label = label
         self.brand = brand
-        self.battery_power = None
-        self.fuel_capacity = None
-        self.max_elec_consumption = 0  # kwh/100Km
-        self.max_fuel_consumption = 0  # L/100Km
-        self.set_energy_capacity(battery_power, fuel_capacity, max_elec_consumption, max_fuel_consumption)
         self._status = None
-        self.abrp_name = None
-        self.set_abrp_name(abrp_name)
-
-    def set_energy_capacity(self, battery_power=None, fuel_capacity=None, max_elec_consumption=None,
-                            max_fuel_consumption=None):
-        if battery_power is not None and fuel_capacity is not None:
-            self.battery_power = battery_power
-            self.fuel_capacity = fuel_capacity
-        elif self.__get_model_name() is not None:
-            model_name = self.__get_model_name()
-            self.battery_power = ENERGY_CAPACITY[model_name]["BATTERY_POWER"]
-            self.fuel_capacity = ENERGY_CAPACITY[model_name]["FUEL_CAPACITY"]
-        else:
-            logger.warning("Can't get car model please check %s", CARS_FILE)
-            self.battery_power = DEFAULT_BATTERY_POWER
-            self.fuel_capacity = DEFAULT_FUEL_CAPACITY
-        if self.is_electric():
-            self.max_fuel_consumption = 0
-        else:
-            self.max_fuel_consumption = max_fuel_consumption or DEFAULT_MAX_FUEL_CONSUMPTION
-        if self.is_thermal():
-            self.max_elec_consumption = 0
-        else:
-            self.max_elec_consumption = max_elec_consumption or DEFAULT_MAX_ELEC_CONSUMPTION
-
-    def __get_model_name(self):
-        if self.label in ENERGY_CAPACITY:
-            return self.label
-        if self.__is_opel_corsa():
-            return "corsa-e"
-        return None
-
-    def __is_opel_corsa(self):
-        return self.brand == "C" and self.label is None
-
-    def set_abrp_name(self, name=None):
-        if name is None:
-            try:
-                self.abrp_name = ENERGY_CAPACITY[self.label]["ABRP_NAME"]
-            except KeyError:
-                self.abrp_name = None
-        else:
-            self.abrp_name = name
+        self.abrp_name = abrp_name or model.abrp_name
+        self.battery_power = battery_power or model.battery_power
+        self.fuel_capacity = fuel_capacity or model.fuel_capacity
+        self.max_elec_consumption = max_elec_consumption or model.max_elec_consumption  # kwh/100Km
+        self.max_fuel_consumption = max_fuel_consumption or model.max_fuel_consumption  # L/100Km
 
     def set_model_name(self, name):
         self.label = name
-        self.set_energy_capacity()
-        self.set_abrp_name()
 
     def is_electric(self) -> bool:
         return self.fuel_capacity == 0 and self.battery_power > 0

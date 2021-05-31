1d95c86b2a352c55f6f00f17f4430fa87f02724e
Florian Bezannier <florian.bezannier@hotmail.fr>
2021-05-31 19:38:36 +0200

    add default options

--- docker_files/init.sh
+++ docker_files/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Containerised psa_car_controller loading..."
-python3 /psa_car_controller/server.py -p $PSACC_PORT -l 0.0.0.0 --web-conf -b $PSACC_BASE_PATH
+python3 /psa_car_controller/server.py -p $PSACC_PORT -l 0.0.0.0 -b $PSACC_BASE_PATH $PSACC_OPTIONS

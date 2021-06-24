d6806772a50912aeaf63b406e3a4485d93461a4e
Florian Bezannier <florian.bezannier@hotmail.fr>
2021-06-24 14:30:26 +0200

    fix args quotes

--- docker_files/init.sh
+++ docker_files/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Containerised psa_car_controller loading..."
 cd "$PSACC_CONFIG_DIR"
-python3 /psa_car_controller/server.py -p $PSACC_PORT -l 0.0.0.0 -b $PSACC_BASE_PATH $PSACC_OPTIONS
+ARGS="-p $PSACC_PORT -l 0.0.0.0 -b $PSACC_BASE_PATH $PSACC_OPTIONS"
+python3 /psa_car_controller/server.py $ARGS

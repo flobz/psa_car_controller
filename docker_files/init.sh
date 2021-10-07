1233e68ba1b3ed64f46aa463e99ac1dda1c754a2
Jens Herrmann <github@jhc.de>
2021-10-07 12:49:50 +0200

    add waiting time
    In some cases the Docker container does not seem to have started properly before the python server starts. This results in the configuration being overwritten.
    See https://github.com/flobz/psa_car_controller/issues/236

--- docker_files/init.sh
+++ docker_files/init.sh
@@ -2,4 +2,5 @@
 echo "Containerised psa_car_controller loading..."
 cd "$PSACC_CONFIG_DIR"
 ARGS="-p $PSACC_PORT -l 0.0.0.0 -b $PSACC_BASE_PATH $PSACC_OPTIONS"
+sleep 30
 python3 -u /psa_car_controller/server.py $ARGS

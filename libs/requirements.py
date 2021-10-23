afd3a4e7816500d8571515493e56a72d9af9bc4b
Florian Bezannier <florian.bezannier@hotmail.fr>
2021-10-23 12:44:19 +0200

    fix test_requirements

--- libs/requirements.py
+++ libs/requirements.py
@@ -1,7 +1,9 @@
+import sys
+
 import pkg_resources
 from pathlib import Path
 
-
+from mylogger import logger
 class TestRequirements:
     """Test availability of required packages."""
 
@@ -12,6 +14,13 @@ class TestRequirements:
         """Test that each required package is available."""
         # Ref: https://stackoverflow.com/a/45474387/
         requirements = pkg_resources.parse_requirements(self.requirement_path.open())
+        missing_requirement = False
         for requirement in requirements:
             requirement = str(requirement)
-            pkg_resources.require(requirement)
+            try:
+                pkg_resources.require(requirement)
+            except pkg_resources.VersionConflict:
+                logger.error("You need to install or update some dependencies: pip install -U %s", requirement)
+                missing_requirement=True
+        if missing_requirement:
+            sys.exit(10)

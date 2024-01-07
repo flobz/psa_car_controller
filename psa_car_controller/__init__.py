c49567a674d71cd0e54e8363c36b414d4a8c9ea9
Florian Bezannier <florian.bezannier@hotmail.fr>
2024-01-07 20:43:49 +0100

    fix: unkown version

--- psa_car_controller/__init__.py
+++ psa_car_controller/__init__.py
@@ -1,9 +1,12 @@
 import sys
+
 if sys.version_info >= (3, 8):
     from importlib import metadata
 else:
     import importlib_metadata as metadata
-
-__version__ = metadata.version(__package__)
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
 
 del metadata, sys

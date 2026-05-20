bed32312f21b8bf2f054ed7e869e3743170d1e51
Florian Bezannier <florian.bezannier@hotmail.fr>
2026-05-20 18:05:56 +0200

    chore: better error log

--- psa_car_controller/psacc/repository/config_repository.py
+++ psa_car_controller/psacc/repository/config_repository.py
@@ -138,7 +138,9 @@ class ElectricityPriceConfig(BaseModel):
                 date = date + timedelta(minutes=30)
             try:
                 res = round(consumption * mean(prices) / self.charger_efficiency, 2)
-            except (TypeError, StatisticsError) as e:
+            except StatisticsError as e:
+                logger.error("Can't get_price of charge: %s-%s prices: %s", start, end, prices, exc_info=e)
+            except TypeError as e:
                 logger.error("Can't get_price of charge, check config", exc_info=e)
         return res
 

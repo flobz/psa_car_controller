cf1881492e717b5ac42f797322a96870de90fafe
Arnoudd <126021001+Arnoudd@users.noreply.github.com>
2026-08-12 17:17:27 +0200

    Apply suggestion from @flobz
    Co-authored-by: Florian BEZANNIER <48728684+flobz@users.noreply.github.com>

--- psa_car_controller/web/view/control.py
+++ psa_car_controller/web/view/control.py
@@ -85,7 +85,7 @@ def get_control_tabs(config):
             )
         )
 
-        return dbc.Tabs(
+    return dbc.Tabs(
             id="control-tabs",
             active_tab="tab-" + config.myp.vehicles_list[0].vin,
             children=tabs,
